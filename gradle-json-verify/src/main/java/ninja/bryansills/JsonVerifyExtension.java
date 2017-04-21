7c5eb215dd3f83b0601a2b6f33d7c46bb14869bb
Bryan Sills <bryan.sills@isobar.com>
2017-04-21 01:03:59 -0500

    Initial Commit

---
+++ gradle-json-verify/src/main/java/ninja/bryansills/JsonVerifyExtension.java
@@ -0,0 +1,14 @@
+package ninja.bryansills;
+
+public class JsonVerifyExtension {
+
+    String path = "/json/";
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
